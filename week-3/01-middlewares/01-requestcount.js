0762eaade326444fcbf6c9442656f014026ef10b
Aashish Anand <anandaashish512@gmail.com>
2023-12-21 19:39:34 +0530

    week3-middlewares

--- week-3/01-middlewares/01-requestcount.js
+++ week-3/01-middlewares/01-requestcount.js
@@ -4,11 +4,18 @@ const express = require('express');
 
 const app = express();
 let requestCount = 0;
+let response=0;
 
 // You have been given an express server which has a few endpoints.
 // Your task is to create a global middleware (app.use) which will
 // maintain a count of the number of requests made to the server in the global
 // requestCount variable
+app.use(function(req,res,next){
+  
+  requestCount=requestCount+1;
+  next();
+
+})
 
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
@@ -22,4 +29,5 @@ app.get('/requestCount', function(req, res) {
   res.status(200).json({ requestCount });
 });
 
+app.listen(3000)
 module.exports = app;
--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -2,19 +2,27 @@ const request = require('supertest');
 const assert = require('assert');
 const express = require('express');
 const app = express();
-// You have been given an express server which has a few endpoints.
-// Your task is to create a global middleware (app.use) which will
-// rate limit the requests from a user to only 5 request per second
-// If a user sends more than 5 requests in a single second, the server
-// should block them with a 404.
-// User will be sending in their user id in the header as 'user-id'
-// You have been given a numberOfRequestsForUser object to start off with which
-// clears every one second
 
 let numberOfRequestsForUser = {};
+
+app.use(function(req, res, next) {
+  const userId = req.headers['user-id'];
+
+  // Initialize the request count for the user if it doesn't exist
+  numberOfRequestsForUser[userId] = numberOfRequestsForUser[userId] || 0;
+
+  if (numberOfRequestsForUser[userId] <= 5) {
+    // Increment the request count for the user
+    numberOfRequestsForUser[userId]++;
+    next();
+  } else {
+    res.status(404).send('Not Found'); // Change to 404 for blocking
+  }
+});
+
 setInterval(() => {
-    numberOfRequestsForUser = {};
-}, 1000)
+  numberOfRequestsForUser = {};
+}, 1000);
 
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
@@ -23,5 +31,7 @@ app.get('/user', function(req, res) {
 app.post('/user', function(req, res) {
   res.status(200).json({ msg: 'created dummy user' });
 });
+
+app.listen(3000);
 
 module.exports = app;
