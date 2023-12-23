165b1b5e9b9cd995d6c25e37822e3e9af9799c52
Aashish Anand <anandaashish512@gmail.com>
2023-12-23 15:38:04 +0530

    Description of the changes

--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -2,17 +2,47 @@ const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
 const router = Router();
 
-// Admin Routes
+// Admin Signup Route
 app.post('/signup', (req, res) => {
     // Implement admin signup logic
+    Admin.create({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.send({
+        message: 'Admin created successfully' 
+    });
 });
 
+// Course Creation Route
 app.post('/courses', adminMiddleware, (req, res) => {
     // Implement course creation logic
+    const { title, description, image, price } = req.body;
+    
+    Course.create({
+        title: title,
+        description: description,
+        image: image,
+        price: price
+    });
+
+    req.send({
+        message: 'Course created successfully'
+    });
 });
 
+// Fetching All Courses Route
 app.get('/courses', adminMiddleware, (req, res) => {
     // Implement fetching all courses logic
+    Course.find()
+        .then(courses => {
+            res.send(courses);
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        });
 });
 
 module.exports = router;
--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -5,10 +5,17 @@ const userMiddleware = require("../middleware/user");
 // User Routes
 app.post('/signup', (req, res) => {
     // Implement user signup logic
+    const {username,password}=req.body
+User.create({
+    username:username,
+    password:password
+})
 });
 
 app.get('/courses', (req, res) => {
     // Implement listing all courses logic
+    
+   
 });
 
 app.post('/courses/:courseId', userMiddleware, (req, res) => {
